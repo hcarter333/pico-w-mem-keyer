9a9eebb0454179e5a69cee38e0d3ddeb574ac65a
antigravkids <hcarter333@gmail.com>
2024-02-05 05:07:59 -0800

    Create simple_key.py

---
+++ simple_key.py
@@ -0,0 +1,12 @@
+import requests
+from time import sleep 
+
+try:
+    while True:
+        msg_s = requests.get('http://10.0.0.170/light/on?msg=cq+cq+de+kd0fnr%2F6+kd0fnr%2F6+kd0fnr%2F6+k')
+        print("msg sent")
+        sleep(27)
+
+except KeyboardInterrupt:
+    print("Exiting...")
+    exit = 1
