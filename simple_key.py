91842d60ecb24b410e187ad144d6be977c702783
antigrav_kids <hcarter333@gmail.com>
2024-02-05 05:38:55 -0800

    increases period

--- simple_key.py
+++ simple_key.py
@@ -5,7 +5,7 @@ try:
     while True:
         msg_s = requests.get('http://10.0.0.170/light/on?msg=cq+cq+de+kd0fnr%2F6+kd0fnr%2F6+kd0fnr%2F6+k')
         print("msg sent")
-        sleep(27)
+        sleep(43)
 
 except KeyboardInterrupt:
     print("Exiting...")
