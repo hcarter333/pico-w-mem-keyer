fa8d519474a121897afbadfdf1713ab54d03875b
antigravkids <hcarter333@gmail.com>
2024-03-02 07:57:25 -0800

    straight key first try

--- keyflex.js
+++ keyflex.js
@@ -1,6 +1,7 @@
 var context = new AudioContext()
 var o = null
 var g = null
+var keycmd = ""
 
 document.addEventListener('DOMContentLoaded', function(){
   $(".js_play_sound").on("mousedown", function(e){
@@ -12,11 +13,13 @@ document.addEventListener('DOMContentLoaded', function(){
   $(".js_play_sound").on("mouseup", function(e){
     e.preventDefault()
     o.stop()
+    keyup()
   })
 
   $(".js_stop_sound").on("click", function(e){
     e.preventDefault()
-    o.stop()
+    //o.stop()
+    //keyup()
   })
 }, false)
 
@@ -26,4 +29,33 @@ function example1(){
   o.type = "sine"
   o.connect(context.destination)
   o.start()
+  keydown()
+}
+
+function keydown(){
+  keycmd = "http://192.168.4.1/light/on?msg=D"
+  keysend()
+}
+
+function keyup(){
+  keycmd = "http://192.168.4.1/light/on?msg=U"
+  keysend()
+}
+
+function keysend(){
+  if (typeof XMLHttpRequest != "undefined") {
+    req = new XMLHttpRequest()
+  } else if (window.ActiveXObject) {
+    req = new ActiveXObject("Microsoft.XMLHTTP")
+  }
+  req.open("GET", keycmd, true)
+  //req.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+        
+  req.onreadystatechange = sk_cb
+  req.send(null);
+
+}
+
+function sk_cb(){
+    return
 }
